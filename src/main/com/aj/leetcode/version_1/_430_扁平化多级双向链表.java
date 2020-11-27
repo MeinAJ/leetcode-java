42a27a750dd81955a4ea8b68c3c78a71b29b8b93
meinaj_foundao <anjun@foundao.com>
2020-11-27 16:56:16 +0800

    更新

---
+++ src/main/com/aj/leetcode/version_1/_430_扁平化多级双向链表.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019, ABB and/or its affiliates. All rights reserved.
+ * ABB PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.aj.leetcode.version_1;
+
+/**
+ * https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/
+ * 多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
+ * 给你位于列表第一级的头节点，请你扁平化列表，使所有结点出现在单级双链表中。
+ * 输入：head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+ * 输出：[1,2,3,7,8,11,12,9,10,4,5,6]
+ *
+ * @author An Jun
+ * @date 2020-11-27
+ */
+public class _430_扁平化多级双向链表 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Node flatten(Node head) {
+
+    }
+
+}
